44fb76b9f9602a0a48770757a306ea7f7e82c918
seok <dreaming.seok@gmail.com>
2021-12-27 21:57:05 +0900

    login complete

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const cookieParser = require("cookie-parser");
 
-const config = require("./config/key")
+const config = require("./config/key");
 
 const { User } = require("./models/User");
 
@@ -15,12 +16,11 @@ app.use(body-parser.json());
 옵션을 작성해야 하지만
 express 4.xx 버전 이상으로는 express에 body-parser 내장
 */
+app.use(cookieParser());
 
 const mongoose = require("mongoose");
 mongoose
-  .connect(
-    config.mongoURI
-  )
+  .connect(config.mongoURI)
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.log(err));
 
@@ -37,6 +37,35 @@ app.post("/register", (req, res) => {
   user.save((err, userInfo) => {
     if (err) return res.json({ success: false, err });
     return res.status(200).json({ success: true });
+  });
+});
+
+app.post("/login", (req, res) => {
+  //요청된 email을 데이터베이스에서 찾는다.
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (!user) {
+      return res.json({
+        loginSuccess: false,
+        message: "이메일이 유효하지 않습니다.",
+      });
+    }
+    //요청된 email이 데이터베이스에 있다면 비밀번호가 맞는지 확인한다.
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (!isMatch)
+        return res.json({
+          loginSuccess: false,
+          message: "비밀번호가 맞지 않습니다.",
+        });
+      //비밀번호가 맞다면 토큰을 생성한다.
+      user.generateToken((err, user) => {
+        if (err) return res.status(400).send(err);
+        //token을 cookie에 저장한다.
+        res
+          .cookie("x_auth", user.token)
+          .status(200)
+          .json({ loginSuccess: true, userId: user._id });
+      });
+    });
   });
 });
 
