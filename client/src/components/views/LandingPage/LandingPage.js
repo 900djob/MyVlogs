0b403c5a976c5c8db6982ee19cea01053cd6952e
seok <dreaming.seok@gmail.com>
2022-01-05 18:58:46 +0900

    login and register complete

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -3,11 +3,24 @@ import axios from "axios";
 
 const LandingPage = () => {
   useEffect(() => {
-    axios.get('/api/hello')
-    .then(res => {console.log(res)})
+    axios.get("/api/hello").then((res) => {
+      console.log(res);
+    });
   }, []);
 
-  return <div>LandingPage</div>;
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100%",
+        height: "100vh",
+      }}
+    >
+      <h2>시작페이지</h2>
+    </div>
+  );
 };
 
 export default LandingPage;
